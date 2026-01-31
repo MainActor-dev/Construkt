73c9ea6a465e17df42358cb6589b5ee2ddc2826a
BK <kurniawan.bayu@hotmail.com>
2026-01-31 20:44:17 +0700

    Implement Networking Layer, MVVM Refactor for User List, and UI Refinements

--- Demo/Construkt/Application/RootViewController.swift
+++ Demo/Construkt/Application/RootViewController.swift
@@ -1,57 +1,50 @@
-//
-//  👨‍💻 Created by @thatswiftdev on 02/11/25.
-//
-//  © 2025, https://github.com/thatswiftdev. All rights reserved.
-//
-//
-//  Permission is hereby granted, free of charge, to any person obtaining a copy
-//  of this software and associated documentation files (the "Software"), to deal
-//  in the Software without restriction, including without limitation the rights
-//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//  copies of the Software, and to permit persons to whom the Software is
-//  furnished to do so, subject to the following conditions:
-//
-//  The above copyright notice and this permission notice shall be included in
-//  all copies or substantial portions of the Software.
-//
-//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-//  THE SOFTWARE.
-
 import UIKit
+import Factory
+import RxSwift
 
 final class RootViewController: UIViewController {
     
+    @Injected(Container.userViewModel) var viewModel: UserViewModel
+    
+    // Tracks the current main view for transition(to:)
+    var mainView: UIView?
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupUI()
-    }
-    
-    private func setupUI() {
-        title = "Construkt"
+        title = "Construkt Users"
         view.backgroundColor = .systemBackground
         
-        let names = ["Alice", "Bob", "Charlie", "David", "Eve", "Frank", "Grace"]
-        
-        view.embed(
-            TableView(
-                DynamicItemViewBuilder(names) { name in
-                    TableViewCell(padding: UIEdgeInsets(top: 6, left: 24, bottom: 6, right: 24)) {
-                        LabelView(name)
-                            .padding(16)
-                            .backgroundColor(.secondarySystemBackground)
-                            .cornerRadius(12)
-                    }
-                }
-            )
-            .separatorStyle(.none)
-        )
+        setupSubscriptions()
     }
+    
+    func setupSubscriptions() {
+        viewModel.$state
+            .observe(on: MainScheduler.instance)
+            .subscribe(onNext: { [weak self] state in
+                guard let self = self else { return }
+                switch state {
+                case .initial:
+                    self.viewModel.load()
+                    
+                case .loading:
+                    self.transition(to: LoadingView())
+                    
+                case .loaded(let users):
+                    self.transition(to: UsersTableView(users: users).reference(&self.mainView))
+                    
+                case .empty(let message):
+                    self.transition(to: EmptyView(message: message))
+                    
+                case .error(let error):
+                    self.transition(to: ErrorView(message: error))
+                }
+            })
+            .disposed(by: disposeBag)
+    }
+    
 }
+
 #if DEBUG
 import SwiftUI
 
---
+++ Demo/Construkt/Application/UserViewModel.swift
@@ -0,0 +1,50 @@
+import Foundation
+import RxSwift
+import RxCocoa
+import Factory
+
+// Assuming Variable is available from the Builder/Construkt module. 
+// If it's internal to the framework, I might need to import the module specifically.
+// Given the file structure, it seems everything is compiled together or 'Construkt' is the module.
+// I'll try without explicit import first, or import Construkt if known.
+
+class UserViewModel {
+    
+    enum State: Equatable {
+        case initial
+        case loading
+        case loaded([User])
+        case empty(String)
+        case error(String)
+    }
+    
+    @Variable private(set) var state = State.initial
+    
+    private let client = NetworkClient(interceptors: [LoggerInterceptor()])
+    private let disposeBag = DisposeBag()
+    
+    func load() {
+        state = .loading
+        
+        Task {
+            do {
+                // Simulate network delay for demo
+                try await Task.sleep(nanoseconds: 500_000_000) // 0.5s
+                
+                let users: [User] = try await client.request(UsersEndpoint.getUsers)
+                
+                await MainActor.run {
+                    if users.isEmpty {
+                        self.state = .empty("No users found.")
+                    } else {
+                        self.state = .loaded(users)
+                    }
+                }
+            } catch {
+                await MainActor.run {
+                    self.state = .error(error.localizedDescription)
+                }
+            }
+        }
+    }
+}
