3fef89645b6fac6d782ec8dbc4f8f8fe91625c57
BK <kurniawan.bayu@hotmail.com>
2026-02-23 12:07:46 +0700

    Update project

--- Sources/Construkt/Core/Builder/Builder+CollectionView.swift
+++ Sources/Construkt/Core/Builder/Builder+CollectionView.swift
@@ -289,40 +289,7 @@ public extension CollectionView {
         return self
     }
     
-    func pagination<B: RxBinding>(
-        model binding: B,
-        threshold: CGFloat = 100,
-        handler: @escaping (Int) -> Void
-    ) -> CollectionView where B.T == ListPaginationModel {
-        
-        let scrollViewObservable =  modifiableView.rx.methodInvoked(#selector(CollectionViewWrapperView.scrollViewDidScroll(_:)))
-            .map { $0.first as? UIScrollView }
-            .compactMap { $0 }
-        
-        let stateObservable = binding.asObservable()
-        
-        scrollViewObservable
-            .throttle(.milliseconds(250), scheduler: MainScheduler.instance)
-            .withLatestFrom(stateObservable) { ($0, $1) }
-            .subscribe(onNext: { (scrollView, model) in
-                guard scrollView.isDragging else { return }
-                
-                let offsetY = scrollView.contentOffset.y
-                let contentHeight = scrollView.contentSize.height
-                let frameHeight = scrollView.frame.height
-                
-                let triggerDistance = contentHeight - frameHeight - threshold
-                
-                if offsetY > triggerDistance {
-                    if !model.isPaginating && !model.isLastPage {
-                        handler(model.nextPage)
-                    }
-                }
-            })
-            .disposed(by: modifiableView.rxDisposeBag)
-        
-        return self
-    }
+
 }
 
 public extension ModifiableView where Base: CollectionViewWrapperView {
